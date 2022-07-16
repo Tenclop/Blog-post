0db52c9804c81be6f23538169a68fec66d93696e
Sihlanic <alin.sihlanic@yahoo.com>
2022-07-16 17:03:29 +0300

    Commit

---
+++ src/SingleComment.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SingleComment = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={props.picture} alt="profile " />
+      </a>
+
+      <div className="content">
+        <a href="/" className="author">
+          {props.name}
+        </a>
+
+        <div className="metadate">
+          <span className="date">{props.date}</span>
+        </div>
+
+        <div className="text">{props.text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleComment;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import profile1 from "./images/person-1.jpg";
+import profile2 from "./images/person-2.jpg";
+import profile3 from "./images/person-3.jpg";
+import SingleComment from "./SingleComment";
+import UserCard from "./UserCard";
+import UserCarad from "./UserCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.querySelector("#root");
+const root = createRoot(rootElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div className="ui comments">
+      <UserCard>
+        <SingleComment
+          name="Tsuda"
+          date="Today at 5:00PM"
+          text="It is amazing"
+          picture={profile1}
+        />
+      </UserCard>
+
+      <UserCard>
+        <SingleComment
+          name="Elise"
+          date="Today at 5:30PM"
+          text="Great job"
+          picture={profile2}
+        />
+      </UserCard>
+
+      <UserCard>
+        <SingleComment
+          name="Jack"
+          date="Today at 7:00PM"
+          text="Thanks.."
+          picture={profile3}
+        />
+      </UserCard>
+    </div>
+  );
+};
+
+root.render(<App />);
+
+//react-nestings
+//jsx
+//react props
